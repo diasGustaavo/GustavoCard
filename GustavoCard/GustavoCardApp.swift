1c12fa64f946d6b0f88766dc0365fb99e73a9491
diasGustaavo <diasgustaavo@icloud.com>
2022-12-23 15:33:19 -0300

    Initial Commit

---
+++ GustavoCard/GustavoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustavoCardApp.swift
+//  GustavoCard
+//
+//  Created by Gustavo Dias on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GustavoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
